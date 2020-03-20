7f4bb390f7df7dfea1fd61623306ff0ea60e1c4c
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-03-20 09:48:45 +0000

    teste

---
+++ CodingStandardSuppress.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+ * @param int $max
+ */
+public function createProgressBar($max = 0): ProgressBar
+{
+    if ($max = 1) {
+
+    }
+}
